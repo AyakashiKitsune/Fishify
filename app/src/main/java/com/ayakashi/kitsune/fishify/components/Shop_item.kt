283e13cda4da2d5dde8859f56382728fcb2268cb
Ayakashi-Kitsune <lancerolan818@gmail.com>
2023-03-09 22:48:52 +0800

    icons fixed

--- app/src/main/java/com/ayakashi/kitsune/fishify/components/Shop_item.kt
+++ app/src/main/java/com/ayakashi/kitsune/fishify/components/Shop_item.kt
@@ -1,9 +1,29 @@
 package com.ayakashi.kitsune.fishify.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
 import com.ayakashi.kitsune.fishify.models.ShopItemModel
 
 @Composable
-fun shop_Item(shopItemModel: ShopItemModel) {
-    
+fun shopItem(shopItemModel: ShopItemModel) {
+    Surface(
+
+    ) {
+        Column {
+            Image(imageVector = shopItemModel.image_Item, contentDescription = null)
+
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun shopItemPreview() {
+    shopItem(shopItemModel = ShopItemModel())
 }
--- app/src/main/java/com/ayakashi/kitsune/fishify/models/Shop_item_model.kt
+++ app/src/main/java/com/ayakashi/kitsune/fishify/models/Shop_item_model.kt
@@ -1,10 +1,12 @@
 package com.ayakashi.kitsune.fishify.models
 
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.ui.graphics.vector.ImageVector
 
 data class ShopItemModel(
-    val name_Item : String = "defaulname",
-    val price_Item : Long = 1000L,
+    val name_Item : String = "defaultname",
+    val price_Item : Double = 20.00,
     val rating_Item : Double = 5.00,
-    val image_Item: ImageBitmap =
+    val image_Item: ImageVector = Icons.Rounded.Person
 )
